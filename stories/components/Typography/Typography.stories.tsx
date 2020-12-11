9548abbe339a719a59484209dc34bb1a6a016080
Cristiano Vinhal <cristiano.vinhal10@gmail.com>
2020-12-11 10:51:49 -0300

    fix storybook

--- stories/components/Typography/Typography.stories.tsx
+++ stories/components/Typography/Typography.stories.tsx
@@ -16,17 +16,17 @@ export const Default = Template.bind({})
 
 const VariationsTemplate: Story<ComponentProps<typeof Typography>> = () => (
   <>
-    <Typography component="h1" color="capri" variant="heading" emphasys>
+    <Typography component="h1" color="primary" variant="heading" emphasys>
       A
     </Typography>
-    <Typography component="h2" color="jet" variant="subheading" emphasys>
+    <Typography component="h2" color="darkGray" variant="subheading" emphasys>
       BEBAS NEUE
     </Typography>
-    <Typography color="dimgray">BOLD</Typography>
-    <Typography color="dimgray">
+    <Typography color="gray">BOLD</Typography>
+    <Typography color="gray">
       A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
     </Typography>
-    <Typography color="dimgray" variant="caption">
+    <Typography color="gray" variant="caption">
       1 2 3 4 5 6 7 8 9 0 ()
     </Typography>
   </>
